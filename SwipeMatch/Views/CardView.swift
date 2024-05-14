344a2018a1f10bb8aa6332a364f927e2067676e1
paulmygame <lapicerospaul@gmail.com>
2024-05-14 20:55:05 +0800

    Add swipe animation

--- SwipeMatch/Views/CardView.swift
+++ SwipeMatch/Views/CardView.swift
@@ -23,9 +23,8 @@ class CardView: UIView {
         let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
         addGestureRecognizer(panGesture)
     }
-            
+    
     @objc fileprivate func handlePan(gesture: UIPanGestureRecognizer) {
-        
         switch gesture.state   {
         case .changed:
             handledChanged(gesture)
@@ -38,13 +37,30 @@ class CardView: UIView {
     
     fileprivate func handledChanged(_ gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: nil)
-        self.transform = CGAffineTransform(translationX: translation.x, y: translation.y )
+        // Rotation
+        let degrees: CGFloat = translation.x / 20
+        let angle = degrees * .pi / 180
+        
+        let rotationalTransformation = CGAffineTransform(rotationAngle: angle)
+        self.transform = rotationalTransformation.translatedBy(x: translation.x, y: translation.y )
     }
     
     fileprivate func handledEnded() {
-        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, animations: { self.transform = .identity})
+        let shouldDismissCard = true
+        
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, animations: {
+            if shouldDismissCard {
+                let offScreenTransform = self.transform.translatedBy(x: 1000, y: 0)
+                self.transform = offScreenTransform
+            } else {
+                self.transform = .identity
+            }
+                
+        }) { (_) in
+            self.transform = .identity
+        }
     }
-    
+     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
